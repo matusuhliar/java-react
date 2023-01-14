40bc48d155cc6d5dbc0971abbf3b00a428936bdd
matus.uhliar@gmail.com <matus.uhliar@gmail.com>
2023-01-14 23:15:04 +0100

    cors

--- backend/src/main/java/sk/uhliar/backend/sample/jwt/WebConfig.java
+++ backend/src/main/java/sk/uhliar/backend/sample/jwt/WebConfig.java
@@ -11,14 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedHeaders("Access-Control-Allow-Origin",
-                        "*",
-                        "Access-Control-Allow-Methods",
-                        "POST, GET, OPTIONS, PUT, DELETE",
-                        "Access-Control-Allow-Headers",
-                        "Origin, X-Requested-With, Content-Type, Accept")
-                .allowedOrigins("*")
-                .allowedMethods("*");
+        registry.addMapping("/**");
     }
+
 }
